0c81164780f5fda28bbb7cf9c34bbdd5330f5e22
MarcoDDM <marantal2013@hotmail.com>
2019-07-05 12:20:59 -0300

    cambios en navegacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './Component/Navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      {/*<Logo />
+        <ImageLinkForm />
+        <FaceRecognition />*/}
     </div>
   );
 }
