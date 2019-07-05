9d81f1026c2613b96bf19173ca5ef9d22fc92568
MarcoDDM <marantal2013@hotmail.com>
2019-07-05 14:07:09 -0300

    modificaciones de logo1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from './Component/Navigation/Navigation';
+import Logo from './Component/Logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
+      <Logo />
+      {/*
         <ImageLinkForm />
         <FaceRecognition />*/}
     </div>
---
+++ src/Component/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+
+const Logo = () => {
+	return (
+		<div className= 'ma4 mt0'>
+			<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+ 				<div className="Tilt-inner"> 👽 </div>
+			</Tilt>
+
+		</div>
+		);
+}
+
+export default Logo; 
