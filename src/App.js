046cb9efab221e5efb99111b3ace2ec007208dd4
MarcoDDM <marantal2013@hotmail.com>
2019-07-07 19:00:45 -0300

    Facerecognition component

--- src/App.js
+++ src/App.js
@@ -135,16 +135,15 @@ class App extends Component {
 
 	onButtonSubmit = () => {
 		console.log('click');
-		app.models
-.predict(
-Clarifai.COLOR_MODEL,
-    // URL
+		app.models.predict(Clarifai.COLOR_MODEL,
     "https://samples.clarifai.com/metro-north.jpg"
 )
 .then(function(response) {
-    // do something with responseconsole.log(response);
+    console.log(response);
     },
-    function(err) {// there was an error}
+    function(err) {
+    	
+    }
 );
 	}
 
---
+++ src/Component/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const FaceRecognition = () => {
+	return (
+		<div>
+		
+		</div>
+		);
+}
+
+export default FaceRecognition; 
