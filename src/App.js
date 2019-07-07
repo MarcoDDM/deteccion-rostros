49cc9f77e241e0f20e8b47145593086e2845d44f
MarcoDDM <marantal2013@hotmail.com>
2019-07-07 14:49:05 -0300

    Agrega funcionalidades al formulario

--- src/App.js
+++ src/App.js
@@ -118,6 +118,16 @@ const particlesOptions = {
 }
 
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+		}
+	}
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	}
+
   render() {
   	return (
     	<div className="App">
@@ -127,7 +137,7 @@ class App extends Component {
       		<Navigation />
       		<Logo />
       		<Rank />
-      		<ImageLinkForm />
+      		<ImageLinkForm onInputChange={this.onInputChange} />
         {/*<FaceRecognition />*/}
     	</div>
   	);
