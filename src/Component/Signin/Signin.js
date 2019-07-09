5fc0e32a2070b7ebdd8c25d206216072b0e083ce
MarcoDDM <marantal2013@hotmail.com>
2019-07-09 16:17:23 -0300

    Hover en p register

--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -21,7 +21,7 @@ const Signin = ({ onRouteChange, }) => {
 	      <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
 	    </div>
 	    <div className="lh-copy mt3">
-	      <p onClick={() => onRouteChange('register')} className="f6 link dim black db">Register</p>
+	      <p onClick={() => onRouteChange('register')} className="f6 link dim pointer black db">Register</p>
 	    </div>
   		</form>
 	</main>
