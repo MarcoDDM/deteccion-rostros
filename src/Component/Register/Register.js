ab64c946b75a4f59352ecb005010e534e5c007a3
MarcoDDM <marantal2013@hotmail.com>
2019-07-09 18:22:10 -0300

    name register

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -22,7 +22,7 @@ const Register = ({ onRouteChange, }) => {
 	      </div>
 	    </fieldset>
 	    <div className="">
-	      <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+	      <input  onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" />
 	    </div>
 	    
   		</form>
