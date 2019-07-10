c7a09921cf27c2cfbcbf4764ac02f2cba65258bc
MarcoDDM <marantal2013@hotmail.com>
2019-07-10 14:30:59 -0300

    Agrega sign in y register

--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-const Navigation = ({ onRouteChange }) => {
-	return (
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+
+		if (isSignedIn) {
+		return(
 		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
 			<p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
 		</nav>
-		);
+		);	
+		} else {
+		return(
+		<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+			<p onClick={() => onRouteChange('signin')} className='f4 link dim black underline pa3 pointer'>Sign In</p>
+			<p onClick={() => onRouteChange('register')} className='f4 link dim black underline pa3 pointer'>Register</p>
+		</nav>
+		
+		)	
+	}
+
 }
 
 export default Navigation; 
