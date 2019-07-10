2d9c2b5aa3137c117ce9f7ba319a0e35c435c997
MarcoDDM <marantal2013@hotmail.com>
2019-07-10 18:48:10 -0300

    solucion de errores con for y class

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -9,15 +9,15 @@ const Register = ({ onRouteChange, }) => {
 	    <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
 	      <legend className="f1 fw6 ph0 mh0">Register</legend>
 	      <div className="mt3">
-	        <label className="db fw6 lh-copy f6" for="email-address">Name</label>
+	        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
 	        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
 	      </div>
 	      <div className="mt3">
-	        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+	        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 	        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
 	      </div>
 	      <div className="mv3">
-	        <label className="db fw6 lh-copy f6" for="password">Password</label>
+	        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 	        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
 	      </div>
 	    </fieldset>
--- src/Component/Signin/Signin.js
+++ src/Component/Signin/Signin.js
@@ -6,14 +6,14 @@ const Signin = ({ onRouteChange, }) => {
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
        <main className="pa4 black-80">
 	  <form className="measure">
-	    <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+	    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 	      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
 	      <div className="mt3">
-	        <label className="db fw6 lh-copy f6" for="email-address">Email</label>
+	        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 	        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
 	      </div>
 	      <div className="mv3">
-	        <label className="db fw6 lh-copy f6" for="password">Password</label>
+	        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 	        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
 	      </div>
 	    </fieldset>
