3e04840b727f24778b8556416e0862d4aa9e1592
MarcoDDM <marantal2013@hotmail.com>
2019-10-21 23:09:47 -0300

    deploy

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -22,7 +22,7 @@ constructor(props) {
   }
 
   onSubmitSignIn = () => {
-  	fetch('http://localhost:3000/register', {
+  	fetch('https://hidden-brushlands-50332.herokuapp.com/register', {
   		method: 'post',
   		headers: {'Content-Type': 'application/json'},
   		body: JSON.stringify({
@@ -33,7 +33,7 @@ constructor(props) {
   	})
   	.then(response => response.json())
   	.then(user => {
-  		if (user) {
+  		if (user.id) {
   		this.props.loadUser(user)
    		this.props.onRouteChange('home'); 			
   		}
@@ -46,7 +46,7 @@ constructor(props) {
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
        <main className="pa4 black-80">
 	  <div className="measure">
-	    <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+	    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 	      <legend className="f1 fw6 ph0 mh0">Registration</legend>
 	      <div className="mt3">
 	        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
